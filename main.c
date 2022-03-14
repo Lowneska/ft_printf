c32d52949c90d7ae084660c66f2a5999c290a889
Lowneska <mira.lisanna@yahoo.fr>
2022-03-14 19:31:19 +0100

    tester finished

--- main.c
+++ main.c
@@ -3,81 +3,93 @@
 
 int	main()
 {
-	int	test_p;
 	int i = 0, j = 0;
 	
-	test_p = 12;
+	int	a = 78;
+	int	*p;
+	p = &a;
+
 	char	*test = "salut";
-	void* pointer = NULL;
-	ft_printf("------------------------------        TEST 1        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%p\n", test);
-	j = printf("Vrai printf :   " "%p\n", test);
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	ft_printf("------------------------------        TEST 2        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%c\n", 'c');
-	j = printf("Vrai printf :   " "%c\n", 'c');
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	ft_printf("------------------------------        TEST 3        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%s\n", "");
-	j = printf("Vrai printf :   " "%s\n", "");
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 4        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%d\n", -2147483647);
-	j = printf("Vrai printf :   " "%d\n", -2147483647);
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 5        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%u\n", (unsigned int)4294967295);
-	j = printf("Vrai printf :   " "%u\n", (unsigned int)4294967295);
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 6        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "Salut a tous!\n");
-	j = printf("Vrai printf :   " "Salut a tous!\n");
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 7        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "Salut a tous!\n");
-	j = printf("Vrai printf :   " "Salut a tous!\n");
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 8        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%x\n", -2147413647);
-	j = printf("Vrai printf :   " "%x\n", -2147413647);
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 9        ------------------------------\n");
-	i = ft_printf("Mon printf :    " "%X\n", -2147413650);
-	j = printf("Vrai printf :   " "%X\n", -2147413650);
-	ft_printf(" mon printf :%d\n", i);
-	printf(" vrai printf :%d\n", j);
-	i = 0;
-	j = 0;
-	ft_printf("------------------------------        TEST 10        ------------------------------\n");
-	ft_printf("                 ce test comporte plusieurs combinaisons de valeurs                \n");
-	ft_printf("===================================================================================\n");
-	ft_printf("                                    Mon printf :    \n" "%d | entier | \n%c | char | \n%s | string \n%x | valeure hexa minuscule | \n%X | valeure hexa majuscule | \n%u | unsigned int | \n ", 341, 'e', "test", 7654, 7654, (unsigned int)4134167265);
-	ft_printf("===================================================================================\n");
-	printf("                                   Vrai printf :   \n" "%d | entier | \n%c | char | \n%s | string \n%x | valeure hexa minuscule | \n%X | valeure hexa majuscule | \n%u | unsigned int | \n ", 341, 'e', "test", 7654, 7654, (unsigned int)4134167265 );
-	ft_printf("-----------      Si tous les résultats sont identiques, c'est bon.      -----------\n");
+	void* p2 = NULL;
 
+	ft_printf("------------------------------        Test 01 : c        ------------------------------\n");
+	i = ft_printf("ft_printf :    %c\n", 'c');
+	j = printf("    printf :   %c\n", 'c');
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
 
-/* A faire 
-	- unsigned
-	- pointeurs*/
+	ft_printf("------------------------------        Test 02 : s        ------------------------------\n");
+	i = ft_printf("ft_printf :    %s\n", "");
+	j = printf("    printf :   %s\n", "");
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 03 : p        ------------------------------\n");
+	i = ft_printf("ft_printf :    %p\n", p2);
+	j = printf("    printf :   %p\n", p2);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 04 : d        ------------------------------\n");
+	i = ft_printf("ft_printf :    %d\n", -2147483647);
+	j = printf("    printf :   %d\n", -2147483647);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+	
+	ft_printf("------------------------------        Test 07 : i        ------------------------------\n");
+	i = ft_printf("ft_printf :    %i\n", -2147483647);
+	j = printf("    printf :   %i\n", -2147483647);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 06 : u        ------------------------------\n");
+	i = ft_printf("ft_printf :    %u\n", (unsigned int)40000000000);
+	j = printf("    printf :   %u\n", (unsigned int)40000000000);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 07 : x        ------------------------------\n");
+	i = ft_printf("ft_printf :    %x\n", -2147413647);
+	j = printf("    printf :   %x\n", -2147413647);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 08 : X        ------------------------------\n");
+	i = ft_printf("ft_printf :    %X\n", -2147413650);
+	j = printf("    printf :   %X\n", -2147413650);
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------        Test 09 : %%      ------------------------------\n");
+	i = ft_printf("ft_printf : %%\n");
+	j = printf("   printf : %%\n");
+	ft_printf("ft_printf :%d\n", i);
+	printf("printf :%d\n", j);
+	i = 0;
+	j = 0;
+
+	ft_printf("------------------------------    Test 10 : simple text   ------------------------------\n");
+	i = ft_printf("ft_printf : Random text\n");
+	j = printf("   printf : Random text\n");
+	ft_printf(" ft_print :%d\n", i);
+	printf(" printf :%d\n", j);
+	i = 0;
+	j = 0;
+
 }
